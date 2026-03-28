46c4af4e38cefbe0f68fcc8a40a10d317f9298fd
Alexandra <alexaaoerozco@gmail.com>
2026-03-28 15:23:32 -0500

    repasando bases-input

---
+++ inputs/name.py
@@ -0,0 +1,18 @@
+print("hello, welcome to the program! ")
+print()
+
+name = (input("what is your name?: ")).strip()
+ 
+print()
+
+print(f"ok {name}, It's a pleasure to meet you!")
+print("Now we're going to do some operations")
+print("today the sum")
+num1 = int(input("enter the first number: "))
+print()
+num2 = int(input("enter ther second number: "))
+
+resultado = num1 + num2
+
+print(f"Ok,The result of your sum is: {resultado}")
+
